8f78cda3280c4e533ffd2851db541daf88feb7f4
Thomas Melchers <thomas.melchers@gmail.com>
2022-03-14 18:17:44 +0100

    adding typography

--- src/assets/customTheme.jsx
+++ src/assets/customTheme.jsx
@@ -1,6 +1,7 @@
 import { createTheme } from '@mui/material'
 import { red } from '@mui/material/colors'
 
+
 const primaryColor = '#CCD7F5' // grey
 const secondaryColor = '#FFC900' // yellow
 const thirdColor = '#0A1A2E' // navy for background
@@ -19,6 +20,11 @@ const customTheme = createTheme({
         error: {
             main: dangerColor
         }
+    },
+
+    typography: {
+        fontFamily: 
+            ['Nunito', 'sans-serif'].join(',')
     }
 })
 
