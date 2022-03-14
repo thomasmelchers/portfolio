d7ea1e50837c46fcfb786aa1fe2d9278183ec06b
Thomas Melchers <thomas.melchers@gmail.com>
2022-03-14 18:18:12 +0100

    adding pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import './App.css';
-import NavBar from './Components/NavBar/NavBar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home';
-
+import React from "react";
+import "./App.css";
+import NavBar from "./Components/NavBar/NavBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import AboutMe from "./Pages/AboutMe";
+import Works from "./Pages/Works";
+import Contact from "./Pages/Contact";
 
 function App() {
   return (
     <div className="App">
-      
       <Router>
-      <NavBar />
-        <Routes>
-          <Route path='/' element={<Home />}></Route>
-        </Routes>
+        <NavBar />
+        <Home />
+        <AboutMe />
+        <Works />
+        <Contact />
+        
+        {/* <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes> */}
       </Router>
     </div>
   );
