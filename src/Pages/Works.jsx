33077d0c3154d10596b0d66b3c17b58c9050dbb9
Thomas Melchers <thomas.melchers@gmail.com>
2022-03-17 22:45:38 +0100

    creation of Works Page

--- src/Pages/Works.jsx
+++ src/Pages/Works.jsx
@@ -1,22 +1,45 @@
 import React from "react";
 import { Box, Grid } from "@mui/material";
 import ArrowDown from "../Components/ArrowDown/ArrowDown";
+import Data from "../Data/Data";
+import Card from "../Components/Card/Card";
+import PageTitle from "../Components/PageTitle/PageTitle";
+import {Link} from 'react-scroll'
 
-const Works = () => {
+const Works = ({id}) => {
+  /* const role = Data.map((x)=>x.role.find((item => {if(item === 'Back-End'){console.log(x.id)}})
+  )) */
+
+  /* const techno = Data.map((x) =>
+    x.technologies.find((item) => {
+      if (item === "React") {
+        console.log(Data[x.id - 1]);
+      }
+    })
+  ); */
   return (
     <Box style={{ backgroundColor: "#0A1A2E" }}>
-      <Grid
-        container
-        style={{ height: "100vh" }}
-        px="10%" /* direction="column" */
-      >
-        <Grid
-          container
-          item
-          mt="6vh"
-          xs={12}
-          sx={{ borderStyle: "dotted", borderColor: "yellow", height: "85vh" }}
-        ></Grid>
+      <Grid container sx={{ height: {xs: '400vh',lg: "100vh"}}} px="10%">
+        <Grid container item mt="6vh" xs={12} sx={{ height: {xs: '380vh', lg: "85vh"}}}>
+
+          <Grid container direction="column" justifyContent='space-around'>
+
+            <Grid item>
+              <Grid container justifyContent="flex-start">
+                <PageTitle
+                  numberTitle={"02."}
+                  pageTitle={"Realisation"}
+                ></PageTitle>
+              </Grid>
+            </Grid>
+
+            <Grid item>
+              <Grid container justifyContent="center" alignItems="center">
+                <Card data={Data} />
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
         <Grid
           container
           justifyContent="center"
@@ -25,7 +48,7 @@ const Works = () => {
           xs={12}
           sx={{ height: "9vh" }}
         >
-          <ArrowDown />
+          <Link to="Contact" spy={true} smooth={true} offset={50} duration={500}><ArrowDown /></Link>
         </Grid>
       </Grid>
     </Box>
