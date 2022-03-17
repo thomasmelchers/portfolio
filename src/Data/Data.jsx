61d3fec4db121571215d32c00952eda3630ce200
Thomas Melchers <thomas.melchers@gmail.com>
2022-03-17 12:36:41 +0100

    adding image to data

--- src/Data/Data.jsx
+++ src/Data/Data.jsx
@@ -1,7 +1,19 @@
+import curryParadise from '../assets/screenshots/curry-paradise.png'
+import cookieClicker from '../assets/screenshots/cookie-clicker.png'
+import dataVisualization from '../assets/screenshots/data-visualization.png'
+import dinoRPG from '../assets/screenshots/dino-rpg.png'
+import codeflix from '../assets/screenshots/codeflix.png'
+import friendlyBook from '../assets/screenshots/friendly-book.png'
+import greenCottage from '../assets/screenshots/greencottage.png'
+import wiki from '../assets/screenshots/wiki.png'
+import { green } from '@mui/material/colors'
+
+
+
 const Data = [
    { id: 1,
         projectName: 'Curry Paradise', 
-        image: 'public/screenshots/curry-paradise.png',
+        image: curryParadise,
         technologies: 'HTML, CSS, Bootstrap',
         description: 'Curry Paradise is my first solo project at BeCode. It is a fake website of a Sri Lankan restaurant. The purpose of this exercice was to practice my HTML and CSS knowledge and to discover Bootstrap',
         github: 'https://github.com/thomasmelchers/restaurant-css-framework',
@@ -11,7 +23,7 @@ const Data = [
     {
         id: 2,
         projectName: 'Cookie Clicker', 
-        image: 'public/screenshots/cookie-clicker.png',
+        image: cookieClicker,
         technologies: 'HTML, CSS, JavaScript',
         description: 'My first team project at BeCode was to create a clone of the well-known cookie-clicker game. As much as click, as much your score will be high and you unlock some bonus.',
         github: 'https://github.com/thomasmelchers/Cookie-clicker',
@@ -20,7 +32,7 @@ const Data = [
     },
     { id: 3,
         projectName: 'Data-Visualisation', 
-        image: 'public/screenshots/data-vizualisation.png',
+        image: dataVisualization,
         technologies: 'HTML, CSS, JavaScript, Ajax, Chart.js',
         description: 'On this project, I have to create three charts and to integrate then into the webpage. The purpose of this exercice was to handle the DOM manipulation, and using some method to fetch data',
         github: 'https://github.com/thomasmelchers/js-datavisualisation-challenge',
@@ -29,7 +41,7 @@ const Data = [
     },
     { id: 4,
         projectName: 'Dino RPG', 
-        image: 'public/screenshots/dino-rpg.png',
+        image: dinoRPG,
         technologies: 'HTML, CSS, JavaScript',
         description: 'I create a RPG where to dinosaurs are fighting eachother. It is a consolidation JS work',
         github: 'https://github.com/thomasmelchers/js-rpg',
@@ -38,7 +50,7 @@ const Data = [
     },
     { id: 5,
         projectName: 'Codeflix', 
-        image: 'public/screenshots/codeflix.png',
+        image: codeflix,
         technologies: 'HTML, CSS, PHP, Bootstrap',
         description: 'Codeflix is a platform where you can discover tutorials about coding. The purpose of this project was to create a dynamic website inspired by Netflix, Amazon or Disney +',
         github: 'https://github.com/thomasmelchers/codeflix',
@@ -47,7 +59,7 @@ const Data = [
     },
     { id: 6,
         projectName: 'FriendlyBook', 
-        image: 'public/screenshots/friendly-book.png',
+        image: friendlyBook,
         technologies: 'React, Node.JS & Express, MongoDB, Mongoose, Material UI',
         description: 'FriendlyBook is a platform directly inspired by Facebook. I create here my first MERN with two fron-end developers.',
         github: 'https://github.com/thomasmelchers/BookFace',
@@ -56,7 +68,7 @@ const Data = [
     },
     { id: 7,
         projectName: 'Green Cottage', 
-        image: 'public/screenshots/greencottage.png',
+        image: greenCottage,
         technologies: 'Node.JS & Express, MongoDB, Mongoose, React, Material UI',
         description: 'Individual project where I create a plateform where users can registrate their accomodation or can book one. The project is still running.',
         github: 'https://github.com/thomasmelchers/npProject',
@@ -65,7 +77,7 @@ const Data = [
     },
     { id: 8,
         projectName: 'Rick and Morty Wiki', 
-        image: 'public/screenshots/wiki.png',
+        image: wiki,
         technologies: 'React',
         description: 'I followed a tutorial to have a better comprehension of React hooks',
         github: 'https://github.com/thomasmelchers/wiki-rick-and-morty',
@@ -76,4 +88,4 @@ const Data = [
 
 
 
-module.exports = {Data}
+export default Data
