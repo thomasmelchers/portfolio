5f6e7ce97c0f087b1542da54005a0dfc98c34bc6
Thomas Melchers <thomas.melchers@gmail.com>
2022-09-08 12:25:08 +0200

    adding experience page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import "./App.css";
 import NavBar from "./Components/NavBar/NavBar";
 import Home from "./Pages/Home";
 import AboutMe from "./Pages/AboutMe";
 import Works from "./Pages/Works";
 import Contact from "./Pages/Contact";
+import Experience from "./Pages/Experience";
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
         <NavBar />
         <section id='Home'><Home /></section>
         <section id='About Me'><AboutMe /></section>
+        <section id='Experience'><Experience /></section>
         <section id='Projects'><Works /></section>
         <section id='Contact'> <Contact /></section>
     </div>
