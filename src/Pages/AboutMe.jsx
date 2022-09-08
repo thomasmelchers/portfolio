a77815a9a80db084f69090e192bf56362d862731
Thomas Melchers <thomas.melchers@gmail.com>
2022-09-08 12:23:13 +0200

    editing text

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -32,15 +32,21 @@ const AboutMe = () => {
                 fontSize="1.2rem"
                 sx={{ display: { xs: "none", lg: "flex" } }}
               >
-                Hello, my name is Thomas. I enjoy creating web applications and
-                playing with data. This interest started a few years ago when I
-                still was a university student. At that time, I devoted my times
-                to work for an NPO for which I created and administrated a
-                website. Its main objective was the promotion of our activities,
-                as social medias only were at their beginnings. Later on, I
-                helped a friend to promote his activities as a tour guide in Sri
-                Lanka by creating an other web application with all the
-                information about his products.{" "}
+                Hello, my name is Thomas. I am a Junior Web Developer and a
+                Historian. After some experiences in the cultural and tourism
+                sector, I have decided to redirect my carrier through web
+                development. This interest started a few years ago when I
+                created several websites for an University NPO or to promote the
+                touristic activities of a friend. In April 2022, I completed a
+                training at BeCode where I have learned the basics thanks to a
+                self-learning and an active pedagogy. During 7 months, I have
+                conceived individual or group projects. I have practiced my
+                skills in a start-up which develop a healthcare solution for
+                patients suffering of cancer or chronic diseases. There I have
+                collaborated with an international team and I have completed
+                some fullstask tasks such as implementing new functionalities on
+                an admin dashboard or integrating a new theme in accord of the
+                UI design.
               </Typography>
               <Typography
                 color="primary"
@@ -49,10 +55,11 @@ const AboutMe = () => {
                 fontSize="1rem"
                 sx={{ display: { lg: "none", xs: "flex" } }}
               >
-                Hello, my name is Thomas. I enjoy creating web applications and playing with data. This interest started a few
-                years ago when I still was a student. I created a website to
-                promote the activities of an NPO where I was involved. Later on, I
-                created a website for a travel guide to promote his activities.
+                Hello, my name is Thomas. I enjoy creating web applications and
+                playing with data. This interest started a few years ago when I
+                still was a student. I created a website to promote the
+                activities of an NPO where I was involved. Later on, I created a
+                website for a travel guide to promote his activities.
               </Typography>
               <Typography
                 color="primary"
@@ -61,45 +68,10 @@ const AboutMe = () => {
                 fontSize="1.2rem"
                 sx={{ display: { xs: "none", lg: "flex" } }}
               >
-                Today, I am completing a training as web developer at BeCode in
-                Brussels. Self-learning and active pedagogy are its two pillars.
-                I have been training myself on individual and group projects. At
-                the beginning, I focused on back-end development and designing
-                databases architecture. After that, I want it to have a better
-                understanding of front-end development. How do I fetch the data?
-                How does React work? Therefore, I have now gained knowledge in
-                both aspects of web development and I am interested in both.
-                Those are the programming languages learned :
+                Today, I am looking for a new challenge where I can use my skills to help a company to develop its digital solution.
               </Typography>
-              <Typography
-                color="primary"
-                textAlign="justify"
-                mt={2}
-                fontSize="1rem"
-                sx={{ display: { lg: "none" } }}
-              >
-                Today, I am completing a training as web developer at BeCode in
-                Brussels. Self-learning and active pedagogy are its two
-                pillars. I have been training myself on individual and group
-                projects. Therefore, I have now gained knowledge in both aspects of
-                development and I am interested in both. Those are the programming languages learned :
-              </Typography>
-              <Grid container justifyContent="center" mt={2}>
-                <Grid item xs={4} sx={{ paddingLeft: {xs: 3, lg: 5}}}>
-                  <Skills skill="HTML 5 & CSS 3" />
-                  <Skills skill="JAVASCRIPT (ES6)" />
-                  <Skills skill="REACT" />
-                </Grid>
-                <Grid item xs={4} sx={{paddingLeft: {xs: 5, lg: 8}}}>
-                  <Skills skill="SQL" />
-                  <Skills skill="GRAPHQL" />
-                </Grid>
-                <Grid item xs={4}>
-                  <Skills skill="NODE & EXPRESS.JS" />
-                  <Skills skill="PHP" />
-                  <Skills skill="MONGDO DB" />
-                </Grid>
-              </Grid>
+              
+              
             </Grid>
             <Grid item xs={12} md={4}></Grid>
           </Grid>
@@ -112,7 +84,13 @@ const AboutMe = () => {
           xs={12}
           sx={{ height: "9vh" }}
         >
-          <Link to="Projects" spy={true} smooth={true} offset={0} duration={500}>
+          <Link
+            to="Experience"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+          >
             <ArrowDown rotate="rotate(-90deg)" />
           </Link>
         </Grid>
