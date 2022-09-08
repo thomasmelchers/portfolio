abb78386a881152a6f04f25c64b0bb0caf7428ad
Thomas Melchers <thomas.melchers@gmail.com>
2022-09-08 12:24:39 +0200

    new style of skills

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,12 +1,21 @@
 import React from "react";
-import { Box, Grid, Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
-const Skills = ({ skill, justifyContent }) => {
+const frame = {
+  borderRadius: '20px',
+  backgroundColor: '#FFC900',
+  padding: {xs: '7px', md: '15px'},
+  paddingRight: {xs: '10px', md: '20px'},
+  marginBottom: '10px',
+  
+}
+
+const Skills = ({ skill }) => {
   return (
-    <Grid container justifyContent="flex-start">
-      <ChevronRightIcon color="secondary" />
-      <Typography color="primary" sx={{fontSize:{xs:'0.7rem', md:'0.9rem', lg:'1rem'}}}> {skill}</Typography>
+    <Grid container justifyContent="flex-start" sx={frame}>
+      <ChevronRightIcon sx={{ fontSize:{xs: '15px', md: '25px'} ,color: '#1B467A'}} />
+      <Typography sx={{fontSize:{xs:'0.7rem', md:'0.9rem', lg:'1rem'}, color: '#0A1A2E', fontWeight: 600}}> {skill}</Typography>
     </Grid>
   );
 };
