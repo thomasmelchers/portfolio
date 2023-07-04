e559e00874b722e846b7a29b4c4365e09c99d1d4
Thomas Melchers <thomas.melchers@gmail.com>
2023-07-04 23:30:50 +0200

    theme

--- src/assets/customTheme.jsx
+++ src/assets/customTheme.jsx
@@ -5,7 +5,7 @@ import { red } from '@mui/material/colors'
 const primaryColor = '#CCD7F5' // grey
 const secondaryColor = '#FFC900' // yellow
 const thirdColor = '#0A1A2E' // navy for background
-const fourthColor = '#FFF89A' // light yellow
+// const fourthColor = '#FFF89A' // light yellow
 const dangerColor = red[900]
 
 const customTheme = createTheme({
