12b4819b75fc45a3b3efc6621f695dc2187caaaa
Thomas Melchers <thomas.melchers@gmail.com>
2023-07-21 20:21:41 +0200

    handling button previous next when modal is opened

--- src/Components/Projects/Carrousel.jsx
+++ src/Components/Projects/Carrousel.jsx
@@ -9,6 +9,7 @@ import Navigation from "./Navigation"
 import ArrowBackIosNewOutlinedIcon from '@mui/icons-material/ArrowBackIosNewOutlined';
 
 import { projectsData } from "../../Data/Data";
+import { SignalCellularNull } from '@mui/icons-material';
 
 const Carrousel = () => {
 
@@ -57,52 +58,70 @@ const handleNext = () => {
   return (
     <div {...handlers}>
       <Grid container alignItem="center" sx={{ height: "100%" }}>
-        <Grid
-          container
-          justifyContent="flex-start"
-          alignItems="center"
-          item
-          display={{ xs: "none", md: "flex" }}
-          md={1}
-        >
-          <Button onClick={handlePrev}><ArrowBackIosNewOutlinedIcon /></Button>
+        
+          <Grid
+            container
+            justifyContent="flex-start"
+            alignItems="center"
+            item
+            display={{ xs: "none", md: isOpen ?  "none" : "flex" }}
+            md={1}
+          >
+            <Button onClick={handlePrev}>
+              <ArrowBackIosNewOutlinedIcon />
+            </Button>
+          </Grid>
+
+
+        <Grid container alignItems="center" item xs={12} md={10} display={{md: isOpen ?  "none" : "flex"}}>
+          <Grid item md={3} display={{ xs: "none", md: "flex" }} sx={cardLeft}>
+            <Card data={projectsData[slideNext()]} />
+          </Grid>
+
+          <Grid
+            item
+            xs={12}
+            md={6}
+            onClick={() => setIsOpen(true)}
+            sx={{ cursor: "pointer" }}
+          >
+            <Card data={projectsData[currentIndex]} />
+          </Grid>
+
+          <Grid item md={3} display={{ xs: "none", md: "flex" }} sx={cardRight}>
+            <Card data={projectsData[slideBefore()]} />
+          </Grid>
         </Grid>
 
+          <Grid
+            container
+            justifyContent="flex-end"
+            alignItems="center"
+            item
+            display={{ xs: "none", md: isOpen ?  "none" : "flex"}}
+            md={1}
+          >
+            <Button onClick={handleNext}>
+              <ArrowBackIosNewOutlinedIcon
+                sx={{ transform: "rotate(180deg)" }}
+              />
+            </Button>
+          </Grid>
 
-        <Grid container alignItems="center" item xs={12} md={10}>
-            <Grid item md={3} display={{ xs: "none", md: "flex" }} sx={cardLeft}>
-                <Card data={projectsData[slideNext()]} />
-            </Grid>
 
-            <Grid item xs={12} md={6} onClick={() => setIsOpen(true)} sx={{cursor: "pointer",}}>
-                <Card data={projectsData[currentIndex]}/>
-            </Grid>
-
-            <Grid item md={3} display={{ xs: "none", md: "flex" }} sx={cardRight}>
-                <Card data={projectsData[slideBefore()]} />
-            </Grid>
-        </Grid>
-
-        <Grid
-          container
-          justifyContent="flex-end"
-          alignItems="center"
-          item
-          display={{ xs: "none", md: "flex" }}
-          md={1}
-        >
-          <Button onClick={handleNext}><ArrowBackIosNewOutlinedIcon sx={{transform: "rotate(180deg)"}}/></Button>
-        </Grid>
-
-        <Grid container alignItems="center" item xs={12} >
-            <Navigation 
+        <Grid container alignItems="center" item xs={12}>
+          <Navigation
             totalSlides={totalSlides}
             currentSlide={currentIndex}
             onBulletClick={(slideIndex) => setCurrentIndex(slideIndex)}
-            />
+          />
         </Grid>
       </Grid>
-      <ModalProjects isOpen={isOpen} setIsOpen={setIsOpen} data={projectsData[currentIndex]}/>
+      <ModalProjects
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        data={projectsData[currentIndex]}
+      />
     </div>
   );
 }
