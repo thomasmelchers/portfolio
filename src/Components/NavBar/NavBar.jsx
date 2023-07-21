c3314219f3ec90fc32bd895380535d1d5012266d
Thomas Melchers <thomas.melchers@gmail.com>
2023-07-21 20:09:58 +0200

    scrolling background effect on NavBar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { AppBar, IconButton, Toolbar, Box, Typography, Tooltip } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
@@ -17,12 +17,29 @@ const Navbar = () => {
     setOpen(false)
   }
 
+  // HANDLING NAVBAR SCROLL
+
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 0;
+      setScrolled(isScrolled);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <header>
-      <AppBar elevation={0}  sx={{ background: "none"}}>
+      <AppBar elevation={0}  sx={{ background: scrolled? "#FFC900"  : "none", opacity:  scrolled? 0.9  : "none"}}>
         <Toolbar style={{ width: "80%", margin: "0 auto" }}>
           <Link to="Home" spy={true} smooth={true} offset={-100} duration={500} style={{ textDecoration: "none", flexGrow: 1, cursor: 'pointer' }}>
-            <Typography color='secondary'
+            <Typography color={scrolled? '#0A1A2E' : 'secondary'  }
               sx={{
                 fontFamily: "Pacifico",
                 fontSize: {xs: '2rem', lg: "2.5rem"},
@@ -41,14 +58,14 @@ const Navbar = () => {
               <Box key={page} ml={3}>
                 <Link to={page} spy={true} smooth={true} offset={0} duration={500} style={{ textDecoration: "none", marginLeft: 2, cursor: 'pointer' }}>
                   <Typography
-                    color='primary'
+                    color={scrolled? "#0A1A2E"  : "primary"}
                     sx={{
                       /* fontFamily: "Fredoka", */
                       fontSize: "1.1rem",
-                      fontWeight: "650",
+                      fontWeight: "800",
                       transition: "0.5s",
                       '&:hover':{
-                        color: 'secondary.main',
+                        color: scrolled? "white"  : "secondary.main",
                         transform: 'scale(1.2)',
                         borderBottom: '3px solid #FFC900'
                       }
@@ -64,7 +81,7 @@ const Navbar = () => {
           <Box sx={{ display: { xs: "flex", lg: "none" } }}>
             <Tooltip title='Open settings'>
               <IconButton onClick={openMenuItem} >
-                <MenuIcon color= 'primary' sx={{fontSize: '2rem', fontWeight: '500'}} />
+                <MenuIcon color= {scrolled? '#0A1A2E' : 'primary'} sx={{fontSize: '2rem', fontWeight: '500'}} />
               </IconButton>
             </Tooltip>
 
